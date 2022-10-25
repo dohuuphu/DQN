fb3bbf5143aa286314e7c72253d6d706199d25fe
Ubuntu <ubuntu@ip-172-31-25-119.ap-southeast-1.compute.internal>
2022-10-25 16:22:25 +0000

    refactor code - bug inference

--- inference.py
+++ inference.py
@@ -5,50 +5,16 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow import keras
-from env_kyon import SimStudent
-from utils.variables import *
+from dqn.model import Agent
+from dqn.environment import SimStudent
+from dqn.variables import *
 
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"   
 
 RANDOM_SEED = 5
 tf.random.set_seed(RANDOM_SEED)
 
-# env = gym.make('CartPole-v1')
-# env.seed(RANDOM_SEED)
-# np.random.seed(RANDOM_SEED)
-
-# print("Action Space: {}".format(env.action_space))
-# print("State space: {}".format(env.observation_space))
-
 env = SimStudent()
-
-class Agent(keras.Model):
-    def __init__(self, state_shape, action_shape, number_topic):
-        super().__init__()
-        initializer = tf.keras.initializers.RandomNormal(mean=0., stddev=1.)
-        init = tf.keras.initializers.HeUniform()
-
-        self.dense1 = keras.layers.Dense(1024, activation=tf.nn.tanh, kernel_initializer=init)
-        self.dense2 = keras.layers.Dense(512, activation=tf.nn.tanh, kernel_initializer=init)
-        self.dense3 = keras.layers.Dense(256, activation=tf.nn.tanh, kernel_initializer=init)
-        self.dense4 = keras.layers.Dense(action_shape, activation='linear', kernel_initializer=init)
-        self.topic_embedding = keras.layers.Embedding(number_topic, 8, input_length=1, trainable=False)
-        # self.dense_topic = keras.layers.Dense(512)
-
-
-    def call(self, inputs):
-        (observation, topic_ids) = inputs
-        topic_embedding = self.topic_embedding(topic_ids)
-        # topic_feature = self.dense_topic(topic_embedding)
-
-        x = self.dense1(observation)
-        # x += topic_embedding
-        # topic_embedding = topic_embedding if topic_embedding.ndim == 2 else tf.expand_dims(topic_embedding, axis=0)
-        x = keras.layers.Concatenate(axis=1)([x, topic_embedding])
-        x = self.dense2(x)
-        x = self.dense3(x)
-        output = self.dense4(x)
-        return output
 
 def infer():
 
